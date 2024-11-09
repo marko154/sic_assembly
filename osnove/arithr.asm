b8d908640210bcda32a6b289a403c746ad7d555e
marko154 <marko.gartnar777@gmail.com>
2024-11-09 17:06:57 +0100

    register only arithmetic

--- osnove/arithr.asm
+++ osnove/arithr.asm
@@ -1,34 +1,35 @@
-primer  START   0
-arithr  LDS     x
-        LDT     y
-        ADDR    S, T
-        STS     sum
-.diff
-        SUBR    S, T
-        SUBR    S, T
-        STS     diff
-.prod
-        LDA     x
-        MUL     y
-        STA     prod
-.quot
-        LDA     x
-        DIV     y
-        STA     quot
-.mod    
-        LDA     quot
-        MUL     y
-        STA     mod
-        LDA     x
-        SUB     mod
-        STA     mod
-halt    J      halt
+. TODO finish using only registers
+primer	START   0
+arithr	LDS     x .sum
+	LDT     y
+	RMO	T, A
+	ADDR    S, A
+	STA     sum
+	.diff
+	RMO	S, A
+	SUBR    T, A
+	STA     diff
+	.prod
+	RMO	T, A
+	MULR    S, A
+	STA     prod
+	.quot
+	LDA     x
+	RMO	S, A
+	DIVR    T, A
+	STA     quot
+	.mod TODO:
+	MULR	T, A
+	SUBR	A, S
+	. SUBR	x*y - (x/y) * y
+	STS	mod
+halt	J      halt
 . podatki
-x       WORD    9
-y       WORD    2
-sum     WORD    0
-diff    WORD    0
-prod    WORD    0
-quot    WORD    0
-mod     WORD    0
-        END    arithr
+x	WORD    9
+y	WORD    2
+sum	WORD    0
+diff	WORD    0
+prod	WORD    0
+quot	WORD    0
+mod	WORD    0
+	END    arithr
